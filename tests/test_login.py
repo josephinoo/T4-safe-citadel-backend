21106dfec5055477cf140a97f3e445e80d9143ff
Joseph Avila Alvarez <josdavilalvarez@gmail.com>
2023-06-23 19:55:18 -0500

    Add ci/cd

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,3 @@
-from unittest.mock import AsyncMock
-
-import pytest
 from fastapi.testclient import TestClient
 
 from main import app
@@ -8,25 +5,9 @@ from main import app
 client = TestClient(app)
 
 
-@pytest.fixture(scope="module")
-def testapp():
-    return client
+# create test login
 
 
-@pytest.mark.asyncio
-async def test_login(testapp, monkeypatch):
-    test_request_payload = {"username": "admin", "password": "admin"}
-    test_response_payload = {"access_token": "string", "refresh_token": "string"}
-
-    async def mock_login(username, password):
-        return 1
-
-    monkeypatch.setattr(
-        "src.crud.login",
-        AsyncMock(
-            side_effect=lambda username, password: mock_login(username, password)
-        ),
-    )
-    response = await testapp.post("/api/login", json=test_request_payload)
-    assert response.status_code == 200
-    assert response.json() == test_response_payload
+async def test_login_unauthorized():
+    response = client.post("/api/login/", json={"username": "foo", "password": "bar"})
+    assert response.status_code == 401
