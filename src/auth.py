33e069526aa1760bbdf318ab9f2259c14588c863
Joseph Avila Alvarez <josdavilalvarez@gmail.com>
2023-09-14 09:56:21 -0500

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -65,8 +65,8 @@ class AuthHandler:
             str: The encoded JWT token.
         """
         payload = {
-            "exp": datetime.now() + timedelta(days=0, minutes=20),
-            "iat": datetime.now(),
+            "exp": datetime.utcnow() + timedelta(days=0, minutes=20),
+            "iat": datetime.utcnow(),
             "sub": str(user_id),
         }
         return jwt.encode(payload, self.secret, algorithm="HS256")
@@ -120,7 +120,7 @@ class AuthHandler:
             str: The refreshed JWT token.
         """
         payload = jwt.decode(token, self.secret, algorithms=["HS256"])
-        payload["exp"] = datetime.now() + timedelta(days=0, minutes=20)
+        payload["exp"] = datetime.utcnow() + timedelta(days=0, minutes=20)
         return jwt.encode(payload, self.secret, algorithm="HS256")
 
     def verify_refresh_token(self, token: str):
